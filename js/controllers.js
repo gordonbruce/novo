36fa82b756842fe32795fe42bd49fa20b03d6b5b
Eduardo Alves <eduardonalves@gmail.com>
2016-04-13 23:26:00 -0300

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('app.controllers', [])
+  
+.controller('inCioCtrl', function($scope) {
+
+})
+   
+.controller('produtosCtrl', function($scope) {
+
+})
+   
+.controller('pedidoCtrl', function($scope) {
+
+})
+      
+.controller('listaDePedidosCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('descontosCtrl', function($scope) {
+
+})
+   
+.controller('chatCtrl', function($scope) {
+
+})
+   
+.controller('localizarEntregaCtrl', function($scope) {
+
+})
+ 
